d6dea5d7b9f32132577cd8038c1f4eb812cc9080
Alex Luna <alexm00n@hotmail.com>
2019-04-22 01:50:08 -0500

    Webpack.config and package.json optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,108 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
+const devMode = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  entry: './src/index.js',
+const config = {
+  mode: devMode ? 'development' : 'production',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './assets/js/[name].bundle.js'
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        include: [path.resolve(__dirname, 'src')],
+        include: path.resolve(__dirname, 'src'),
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              [
-                'emotion',
-                {
-                  // sourceMap is on by default but source maps are dead code eliminated in production
-                  sourceMap: false,
-                  autoLabel: process.env.NODE_ENV !== 'production',
-                  labelFormat: '[local]',
-                  cssPropOptimization: true
-                }
-              ]
-            ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.html$/,
+        use: ['html-loader']
       },
       {
-        test: /\.(png|jpg|gif|eot|ttf|otf|woff)$/,
-        use: ['file-loader']
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/media/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/fonts/'
+            }
+          }
+        ]
+      },
+      // {
+      //   test: /\.(sa|sc|c)ss$/,
+      //   use: ['style-loader', 'css-loader']
+      // }
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development'
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: process.env.NODE_ENV === 'development'
+            }
+          }
+        ]
       }
     ]
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode
+        ? './assets/css/[name].css'
+        : './assets/css/[name].[hash].css',
+      chunkFilename: devMode
+        ? './assets/css/[id].css'
+        : './assets/css/[id].[hash].css'
+    })
+  ],
+  devServer: {
+    index: 'index.html',
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    stats: 'normal',
+    open: 'chrome',
+    writeToDisk: true
+  },
+  devtool: 'source-map'
 }
+
+module.exports = config
